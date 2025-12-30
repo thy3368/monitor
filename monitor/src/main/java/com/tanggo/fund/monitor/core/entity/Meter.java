40dafe4d4d082f6ffeeca6e948108269b575650f
thy3368 <thy_email@126.com>
2025-12-30 18:46:15 +0800

    monitor插件化

--- monitor/src/main/java/com/tanggo/fund/monitor/core/entity/Meter.java
+++ monitor/src/main/java/com/tanggo/fund/monitor/core/entity/Meter.java
@@ -1,4 +1,16 @@
 package com.tanggo.fund.monitor.core.entity;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
 public class Meter {
+    private String meterId;           // 指标ID
+    private LocalDateTime timestamp;   // 采集时间
+    private String metricName;        // 指标名称
+    private double metricValue;       // 指标值
+    private Map<String, String> tags; // 标签（用于分类和聚合）
+//    private MeterStatus status;       // 指标状态（正常、告警等）
 }
