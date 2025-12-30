a18acac40c4d8e0e777e0bed63ba839afcf80c5f
thy3368 <thy_email@126.com>
2025-12-30 17:41:56 +0800

    monitor插件化

--- monitor/src/main/java/com/tanggo/fund/monitor/adapter/channel/SSHMonitorChannel.java
+++ monitor/src/main/java/com/tanggo/fund/monitor/adapter/channel/SSHMonitorChannel.java
@@ -1,16 +1,19 @@
 package com.tanggo.fund.monitor.adapter.channel;
 
 import com.tanggo.fund.monitor.core.entity.MonitorChannel;
+import com.tanggo.fund.monitor.core.entity.meta.ChannelMeta;
+import com.tanggo.fund.monitor.core.entity.meta.CommandMeta;
 
 public class SSHMonitorChannel implements MonitorChannel {
 
+
     @Override
-    public void connect() {
+    public void connect(ChannelMeta channelMeta) {
 
     }
 
     @Override
-    public String execute(String cmd) {
+    public String execute(CommandMeta cmd) {
         return "";
     }
 }
